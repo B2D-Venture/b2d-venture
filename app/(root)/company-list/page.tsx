b648c657b58616ef566c3d78a9ac24560f94f990
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-09-03 10:29:51 +0700

    Make company card component responsive

--- app/(root)/company-list/page.tsx
+++ app/(root)/company-list/page.tsx
@@ -3,21 +3,24 @@ import CompanyCard from "@/components/CompanyCard";
 import { companyExample } from "@/constants";
 
 const CompanyList = () => {
+  const repeatedCompanies = [].concat(companyExample, companyExample, companyExample);
+
   return (
     <div>
       CompanyList
-      <div className="flex flex-row m-10">
-        {companyExample.map((company) => (
-          <CompanyCard
-            key={company.companyName}
-            logoUrl={company.logoUrl}
-            backgroundUrl={company.backgroundUrl}
-            companyName={company.companyName}
-            shortDescription={company.shortDescription}
-            investmentGoal={company.investmentGoal}
-            investorCount={company.investorCount}
-            minInvest={company.minInvest}
-          />
+      <div className="flex flex-wrap m-10">
+        {repeatedCompanies.map((company, index) => (
+          <div key={`${company.companyName}-${index}`} className="flex-1">
+            <CompanyCard
+              logoUrl={company.logoUrl}
+              backgroundUrl={company.backgroundUrl}
+              companyName={company.companyName}
+              shortDescription={company.shortDescription}
+              investmentGoal={company.investmentGoal}
+              investorCount={company.investorCount}
+              minInvest={company.minInvest}
+            />
+          </div>
         ))}
       </div>
     </div>
