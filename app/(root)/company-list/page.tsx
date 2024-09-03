7e1a63da23a39a07d0c9dd606ef8562d05714a80
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-09-03 18:40:02 +0700

    Add design for company search filter

--- app/(root)/company-list/page.tsx
+++ app/(root)/company-list/page.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import CompanyCard from "@/components/CompanyCard";
 import { companyExample } from "@/constants";
+import { FaSearch } from "react-icons/fa";
+import { IoFilter } from "react-icons/io5";
 
 const CompanyList = () => {
-  const repeatedCompanies = [].concat(companyExample, companyExample, companyExample);
+  const repeatedCompanies = [].concat(
+    companyExample,
+    companyExample,
+    companyExample
+  );
 
   return (
     <div>
-      CompanyList
+      <div className="search-filter mx-32 my-14 group focus-within:border-yellow-500 focus-within:border-4">
+        <FaSearch className="mr-3" />
+        <input
+          type="text"
+          className="flex-1 focus:outline-none focus:ring-0"
+          placeholder="Search for companies"
+        />
+        <IoFilter className="ml-3" />
+      </div>
       <div className="flex flex-wrap m-10">
         {repeatedCompanies.map((company, index) => (
           <div key={`${company.companyName}-${index}`} className="flex-1">
