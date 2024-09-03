4ea99e231a5ec4f7f4655191eb8fa9b4ce591395
Natthapol SERMSARAN <natthapolwork@gmail.com>
2024-09-03 21:00:37 +0700

    Add layout and image for the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,42 @@
+import CompanyLogoList from "@/components/CompanyLogoList";
 import React from "react";
+import Image from "next/image";
+import CompanyAllStatistics from "@/components/CompanyAllStatistics";
 
 const Home = () => {
   return (
-    <div>
-      HOME
+    <div className="w-[1440px] h-[1967px] relative bg-[#181a20]">
+      <Image
+        className="max-w-full h-auto left-0 top absolute"
+        src="/img-main/background-img-main.png"
+        alt="Background"
+        width={1440}
+        height={630}
+      />
+      <div className="w-[731px] h-[479px] left-[71px] top-[58px] absolute">
+        <span className="text-white text-[64px] font-bold font-['Inter']">
+          Invest for Future in
+          <br></br>
+          Stable Platform and
+        </span>
+        <br></br>
+        <span className="text-[#e49012] text-[64px] font-bold font-['Inter']">
+          Make Fast Money
+        </span>
+      </div>
+      <div className="w-[1458px] h-[254px] left-[-18px] top-[630px] absolute bg-[#121212]/80" />
+        <CompanyAllStatistics />
+      <div className="w-[500px] h-[479px] left-[41px] top-[916px] absolute text-white text-4xl font-bold font-['Inter']">
+        2,500+ businesses funded and served
+      </div>
+      <div className="left-[41px] top-[1478px] absolute text-white text-4xl font-bold font-['Inter']">
+        TOP 10 Companies
+      </div>
+      <div className="w-[235px] h-[17px] left-[604px] top-[2025px] absolute justify-center items-center inline-flex">
+        <div className="w-[235px] h-[17px] text-white text-sm font-normal font-['Inter']">
+          copyright ©2024. All rights reserve
+        </div>
+      </div>
     </div>
   );
 };
