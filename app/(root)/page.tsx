af60312e2d2623b3ace8476e7452233bdf2f7f81
Natthapol SERMSARAN <natthapolwork@gmail.com>
2024-09-03 22:25:00 +0700

    Add CompanyLogoBoxList to main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
-import CompanyLogoList from "@/components/CompanyLogoList";
 import React from "react";
 import Image from "next/image";
 import CompanyAllStatistics from "@/components/CompanyAllStatistics";
+import CompanyLogoBoxList from "@/components/CompanyLogoBoxList";
 
 const Home = () => {
   return (
@@ -25,11 +25,12 @@ const Home = () => {
         </span>
       </div>
       <div className="w-[1458px] h-[254px] left-[-18px] top-[630px] absolute bg-[#121212]/80" />
-        <CompanyAllStatistics />
-      <div className="w-[500px] h-[479px] left-[41px] top-[916px] absolute text-white text-4xl font-bold font-['Inter']">
+      <CompanyAllStatistics />
+      <div className="left-[41px] top-[916px] absolute text-white text-4xl font-bold font-['Inter']">
         2,500+ businesses funded and served
+        <CompanyLogoBoxList />
       </div>
-      <div className="left-[41px] top-[1478px] absolute text-white text-4xl font-bold font-['Inter']">
+      <div className="left-[41px] top-[1378px] absolute text-white text-4xl font-bold font-['Inter']">
         TOP 10 Companies
       </div>
       <div className="w-[235px] h-[17px] left-[604px] top-[2025px] absolute justify-center items-center inline-flex">
