a325097d309e86e568ae6d50409bdf287d918554
Peanutt47 <pnchalee@gmail.com>
2024-09-09 11:34:44 +0700

    add imageform

--- components/CompanyRegisterForm.tsx
+++ components/CompanyRegisterForm.tsx
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
 import {
   Form,
   FormControl,
@@ -13,14 +15,14 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
 
+import { ImageForm } from "@/components/UploadImageForm";
 
 const formSchema = z.object({
   CompanyName: z.string(),
-  Abbreviation: z.string()
-})
+  Abbreviation: z.string(),
+});
 
 export function ProfileForm() {
   // 1. Define your form.
@@ -28,43 +30,49 @@ export function ProfileForm() {
     resolver: zodResolver(formSchema),
     defaultValues: {
       CompanyName: "",
-      Abbreviation: ""
+      Abbreviation: "",
     },
-  })
+  });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    console.log(values);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="CompanyName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Name</FormLabel>
-              <FormControl>
-                <Input placeholder="Fullname" {...field} />
-              </FormControl>
-            </FormItem>
-          )}
-        />
-        <FormField control={form.control} name="Abbreviation" render={({ field }) => (
-                      <FormItem>
-                      <FormLabel>Abbreviation</FormLabel>
-                      <FormControl>
-                        <Input placeholder="GOOG" {...field} />
-                      </FormControl>
-                    </FormItem>
-        )}
-        />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 ">
+        <div className="flex gap-4">
+          <ImageForm/>
+          <FormField
+            control={form.control}
+            name="CompanyName"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="Fullname" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="Abbreviation"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Abbreviation</FormLabel>
+                <FormControl>
+                  <Input placeholder="GOOG" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-  )
+  );
 }
---
+++ components/UploadImageForm.tsx
@@ -0,0 +1,36 @@
+import { Label } from "@/components/ui/label";
+
+export function ImageForm() {
+  return (
+    <>
+      <div className="relative">
+        <input
+          id="picture"
+          type="file"
+          className="absolute inset-0 opacity-0 cursor-pointer"
+          onChange={(e) => {
+            const file = e.target.files?.[0];
+            if (file) {
+              const reader = new FileReader();
+              reader.onloadend = () => {
+                const imgElement = document.getElementById(
+                  "preview-image"
+                ) as HTMLImageElement;
+                if (imgElement) imgElement.src = reader.result as string;
+              };
+              reader.readAsDataURL(file);
+            }
+          }}
+        />
+        <div className="w-40 h-40 bg-gray-200 rounded-full overflow-hidden flex items-center justify-center text-center">
+          <img
+            id="preview-image"
+            src=""
+            alt="Logo"
+            className="w-full h-full object-cover"
+          />
+        </div>
+      </div>
+    </>
+  );
+}
