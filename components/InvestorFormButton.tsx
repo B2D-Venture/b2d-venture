48a76aabaf1af27787836ccb946d8f9de4db9b20
Natthapol SERMSARAN <natthapolwork@gmail.com>
2024-09-11 02:06:46 +0700

    Add component investor form button

---
+++ components/InvestorFormButton.tsx
@@ -0,0 +1,5 @@
+const InvestorFormButton = ({ label }: InvestorFormButtonProps) => (
+  <button className="w-full bg-black text-white p-4 rounded-lg">{label}</button>
+);
+
+export default InvestorFormButton;
