c80de32c3f6113e3e0636c2dc9f2d4ae896d0977
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-09-15 23:31:15 +0700

    Make logo, name, and short description responsive on small screens

--- app/(root)/company-profile/page.tsx
+++ app/(root)/company-profile/page.tsx
@@ -13,41 +13,40 @@ const CompanyProfile = () => {
           className="rounded-[5px]"
         />
       </div>
-      <div className="w-full h-[100px]">
-        <div className="logo relative w-[200px] h-[200px] top-40% left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      <div className="w-full h-[70px] md:h-[100px] lg:h-[80px]">
+        <div className="logo relative w-[120px] h-[120px] top-40% left-1/2 transform -translate-x-1/2 -translate-y-1/2 xl:w-[200px] xl:h-[200px] lg:w-[170px] lg:h-[170px] md:w-[150px] md:h-[150px]">
           <Image
             src="https://static.wixstatic.com/media/d2252d_4c1a1bda6a774bd68f789c0770fd16e5~mv2.png"
             alt="logo"
             layout="fill"
-            objectFit="cover"
             className="rounded-[5px]"
           />
         </div>
       </div>
-      <p className="name text-5xl text-white left-1/2 text-center mt-3">
+      <p className="name text-2xl text-white left-1/2 text-center md:mt-8 md:text-5xl">
         Company name
       </p>
-      <div className="detail text-center text-white text-xl mt-3">
+      <div className="detail text-center text-white text-sm mt-3 md:text-xl">
         is a startup that is developing a new generation of plants that are more
         sustainable, efficient, and beautiful than ever before.
       </div>
-      <div className="ml-20 mr-20">
+      <div className="mx-8 mt-4 md:mx-20">
         <div className="flex justify-between mb-1">
-          <span className="text-base font-medium text-[#fcd535] dark:text-yellow-500">
+          <span className="text-sm md:text-base font-medium text-[#fcd535] dark:text-yellow-500">
             45% - 30 days left
           </span>
-          <span className="text-base font-medium text-[#fcd535] dark:text-white">
+          <span className="text-sm md:text-base font-medium text-[#fcd535] dark:text-white">
             $ 100,000 target
           </span>
         </div>
         <div className="w-full bg-gray-200 rounded-full h-8 mb-4 dark:bg-gray-700">
           <div className="bg-yellow-400 h-8 rounded-full w-[45%] text-black flex items-center">
-            <div className="ml-5">$45,000 from 22 investors</div>
+            <div className="ml-2 md:ml-5 text-sm md:text-base">$45,000 from 22 investors</div>
           </div>
         </div>
       </div>
       <div className="grid mt-10 grid-cols-2 h-full w-full text-white">
-        <div className="flex relative ml-[80px]">
+        <div className="flex relative ml-8 md:ml-[80px]">
           <div className="Highlights absolute text-5xl top-[20px] left-[0px]">
             Highlights
           </div>
