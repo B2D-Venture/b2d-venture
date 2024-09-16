509e30c2914b1865386a0e99f077e28a40f4dcf6
Peanutt47 <pnchalee@gmail.com>
2024-09-16 11:58:56 +0700

    create InvestorProfileBadge

---
+++ components/admin/badge/InvestorProfileBadge.tsx
@@ -0,0 +1,11 @@
+import { Badge } from "@/components/ui/badge"
+
+const InvestorProfileBadge = () => {
+  return (
+    <>
+    <Badge className="bg-[#3bb8b6] text-black">Investor Profile</Badge>
+    </>
+  );
+}
+
+export default InvestorProfileBadge;
