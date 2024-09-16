032a63358559430c1cb04a2da55cba3251061961
Peanutt47 <pnchalee@gmail.com>
2024-09-16 11:57:13 +0700

    create CompanyProfileBadge

---
+++ components/admin/badge/CompanyProfileBadge.tsx
@@ -0,0 +1,11 @@
+import { Badge } from "@/components/ui/badge"
+
+const CompanyProfileBadge = () => {
+  return (
+    <>
+    <Badge className="bg-[#fcd535] text-black">Company Profile</Badge>
+    </>
+  );
+}
+
+export default CompanyProfileBadge;
