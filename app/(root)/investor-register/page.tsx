a185f73d84212ff55a15304e7bacbff7384357e0
Natthapol SERMSARAN <natthapolwork@gmail.com>
2024-09-16 10:16:26 +0700

    Add margin to investor register page

--- app/(root)/investor-register/page.tsx
+++ app/(root)/investor-register/page.tsx
@@ -5,7 +5,7 @@ export default function InvestorRegister() {
     <div>
       <div className="flex flex-col items-center min-h-screen mt-7">
         <div className="flex w-11/12 h-9/10">
-          <h1 className="self-start text-white ml-70 text-[40px] font-bold">
+          <h1 className="self-start text-white ml-70 text-[40px] font-bold mb-2">
             Investor Profile
           </h1>
         </div>
