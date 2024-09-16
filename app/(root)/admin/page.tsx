e919ce6f047831dc2f8788f60c5f02a70c556429
Peanutt47 <pnchalee@gmail.com>
2024-09-17 01:25:35 +0700

    create company profile card

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import DealBadge from '@/components/admin/badge/DealBadge'
-import CompanyProfileBadge from '@/components/admin/CompanyProfile/CompanyProfileBadge'
-import InvestorProfileBadge from '@/components/admin/badge/InvestorProfileBadge'
-import CompanyProfileCard from '@/components/admin/CompanyProfile/CompanyProfileCard'
+import React from "react";
+import DealBadge from "@/components/admin/badge/DealBadge";
+import CompanyProfileBadge from "@/components/admin/CompanyProfile/CompanyProfileBadge";
+import InvestorProfileBadge from "@/components/admin/badge/InvestorProfileBadge";
+import { CompanyProfileCard } from "@/components/admin/CompanyProfile/CompanyProfileCard";
 
 const AdminPage = () => {
   return (
-    <div className='text-white flex flex-col items-center mt-10'>
-      <CompanyProfileCard />
+    <div className="text-white flex flex-col items-center mt-10">
+      <div className="flex w-11/12 h-11/12 bg-[#D9D9D9] rounded-[10px] justify-center items-center p-[40px]">
+        <CompanyProfileCard
+          logo="https://static.wixstatic.com/media/d2252d_4c1a1bda6a774bd68f789c0770fd16e5~mv2.png"
+          companyName="Amazon"
+          ceoName="Kammueang Salengdoi"
+          description="Amazon.com, Inc. is an American multinational technology company which focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence."
+          valuation={1000000}
+          income={100000}
+          profit={10000}
+          employees={1000}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
--- components/admin/CompanyProfile/CompanyProfileCard.tsx
+++ components/admin/CompanyProfile/CompanyProfileCard.tsx
@@ -4,6 +4,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
 
 import * as React from "react";
 
@@ -19,17 +20,89 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const CompanyProfileCard = () => {
-  return (
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
-  );
-};
+import CompanyProfileBadge from "@/components/admin/CompanyProfile/CompanyProfileBadge";
 
-export default CompanyProfileCard;
+export function CompanyProfileCard({
+  logo,
+  companyName,
+  description,
+  ceoName,
+  valuation,
+  income,
+  profit,
+  employees,
+}: {
+  logo: string;
+  companyName: string;
+  description: string;
+  ceoName: string;
+  valuation: number;
+  income: number;
+  profit: number;
+  employees: number;
+}) {
+  return (
+    <div className="flex flex-col w-full space-y-5">
+      <Accordion type="single" collapsible className="w-full">
+        <AccordionItem value="item-1">
+          <div className="flex flex-row space-x-10">
+            <img
+              src={logo}
+              alt="Company Logo"
+              className="w-14 h-14 rounded-full"
+            />
+            <AccordionTrigger className="text-lg text-black flex w-full justify-between">
+              <span className="flex-1">{companyName}</span>
+              {/* <ChevronDownIcon
+                className="AccordionChevron text-muted-foreground transition-transform duration-200 text-black"
+                aria-hidden
+              /> */}
+            </AccordionTrigger>
+          </div>
+          <div className="border-b-2 border-black mt-2 mb-2" />
+          <CompanyProfileBadge />
+          <p className="w-full overflow-hidden text-black pt-2">
+            {description}
+          </p>
+          <AccordionContent>
+            <div className="flex flex-row items-center justify-center space-x-5">
+              <Label>Valuation</Label>
+              <div className="flex flex-row items-center space-x-1">
+                <span>$</span>
+                <Input
+                  value={valuation}
+                  disabled
+                  className="border-2 border-black w-auto"
+                />
+              </div>
+              <Label>Income</Label>
+              <div className="flex flex-row items-center space-x-1">
+                <span>$</span>
+                <Input
+                  value={income}
+                  disabled
+                  className="border-2 border-black w-auto"
+                />
+              </div>
+              <Label>Profit</Label>
+              <div className="flex flex-row items-center space-x-1">
+                <span>$</span>
+                <Input
+                  value={profit}
+                  disabled
+                  className="border-2 border-black w-auto"
+                />
+              </div>
+              <Label>Employees</Label>
+              <Input
+                value={employees}
+                disabled
+                className="border-2 border-black"
+              />
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
