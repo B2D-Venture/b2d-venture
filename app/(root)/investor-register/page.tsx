f54bec06211347797ba8417aa8317ce5ac415628
Natthapol SERMSARAN <natthapolwork@gmail.com>
2024-09-16 09:23:05 +0700

    Update investor register page

--- app/(root)/investor-register/page.tsx
+++ app/(root)/investor-register/page.tsx
@@ -3,7 +3,7 @@ import { InvestorRegisterForm } from "@/components/InvestorRegisterForm";
 export default function InvestorRegister() {
   return (
     <div>
-      <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="flex flex-col items-center min-h-screen mt-7">
         <div className="flex w-11/12 h-9/10">
           <h1 className="self-start text-white ml-70 text-[40px] font-bold">
             Investor Profile
