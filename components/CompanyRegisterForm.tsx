dfc4a8aefde8a07fe2228043b0340c3e7e21bdcd
Peanutt47 <pnchalee@gmail.com>
2024-09-16 10:26:36 +0700

    change path file

--- components/CompanyRegisterForm.tsx
+++ components/CompanyRegisterForm.tsx
@@ -7,7 +7,7 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ProfileImageForm } from "@/components/UploadImageForm";
+import { ProfileImageForm } from "@/components/ProfileImageForm";
 import { CalendarForm, CalendarFormSchema } from "@/components/CalendarForm";
 import { BannerImageForm } from "@/components/BannerImageForm";
 
