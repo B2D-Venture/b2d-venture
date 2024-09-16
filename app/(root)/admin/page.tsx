7e046dcad2ae514fbc35e8c36bea1b3e89798b9f
Peanutt47 <pnchalee@gmail.com>
2024-09-16 23:52:34 +0700

    add install component

--- app/(root)/admin/page.tsx
+++ app/(root)/admin/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import DealBadge from '@/components/admin/badge/DealBadge'
+import CompanyProfileBadge from '@/components/admin/CompanyProfile/CompanyProfileBadge'
+import InvestorProfileBadge from '@/components/admin/badge/InvestorProfileBadge'
+import CompanyProfileCard from '@/components/admin/CompanyProfile/CompanyProfileCard'
 
 const AdminPage = () => {
   return (
     <div className='text-white flex flex-col items-center mt-10'>
-        <h1>1</h1>
-        <h2>2</h2>
-        <DealBadge/>
+      <CompanyProfileCard />
     </div>
   )
 }
---
+++ components/admin/CompanyProfile/CompanyProfileCard.tsx
@@ -0,0 +1,35 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const CompanyProfileCard = () => {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default CompanyProfileCard;
