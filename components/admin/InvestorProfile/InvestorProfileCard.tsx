4903f775a7772f6de1354a15678df76e569a2492
Peanutt47 <pnchalee@gmail.com>
2024-09-17 13:04:31 +0700

    convert to component and add email, Nationality

--- components/admin/InvestorProfile/InvestorProfileCard.tsx
+++ components/admin/InvestorProfile/InvestorProfileCard.tsx
@@ -11,18 +11,23 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 import InvestorProfileBadge from "@/components/admin/InvestorProfile/InvestorProfileBadge";
+import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { OutputTextBox } from "@/components/admin/OutputTextBox";
+import { OutputTextBoxDollar } from "../OutputTextBoxDollar";
 
 export function InvestorProfileCard({
   logo,
   investorName,
-  description,
+  email,
   age,
   netWorth,
   moneyReadyForInvestment,
+  Nationality,
 }: {
   logo: string;
   investorName: string;
-  description: string;
+  email: string;
+  Nationality: string;
   age: number;
   netWorth: number;
   moneyReadyForInvestment: number;
@@ -31,53 +36,60 @@ export function InvestorProfileCard({
     <div className="flex flex-col w-full space-y-5">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="item-1">
-          <div className="flex flex-row space-x-10">
+          <div className="grid grid-cols-10">
             <img
               src={logo}
               alt="Investor Profile"
-              className="w-14 h-14 rounded-full"
+              className="w-14 h-14 rounded-full col-span-1"
             />
-            <AccordionTrigger className="text-lg text-black flex w-full justify-between">
-              <span className="flex-1">{investorName}</span>
-              {/* <ChevronDownIcon
-                className="AccordionChevron text-muted-foreground transition-transform duration-200 text-black"
-                aria-hidden
-              /> */}
-            </AccordionTrigger>
+            <div className="col-span-9">
+              <AccordionTrigger className="text-lg text-black flex w-full justify-between text-left">
+                <span className="flex-1">{investorName}</span>
+                <ChevronDownIcon
+                  className="text-muted-foreground transition-transform duration-200"
+                  aria-hidden
+                  color="black"
+                  scale={1.5}
+                />
+              </AccordionTrigger>
+            </div>
           </div>
           <div className="border-b-2 border-black mt-2 mb-2" />
           <InvestorProfileBadge />
-          <p className="w-full overflow-hidden text-black pt-2">
-            {description}
-          </p>
+          <div className="flex flex-row items-center space-x-4 mt-2 text-black mb-2">
+            <OutputTextBox
+              label="Email"
+              value={email}
+              classNameLabel=""
+              classNameValue=""
+            />
+            <OutputTextBox
+              label="Nationality"
+              value={Nationality}
+              classNameLabel=""
+              classNameValue=""
+            />
+          </div>
           <AccordionContent>
             <div className="flex flex-row items-center justify-between">
-              <Label>Age</Label>
-              <div className="flex flex-row items-center space-x-1">
-                <Input
-                  value={age}
-                  disabled
-                  className="border-2 border-black w-auto"
-                />
-              </div>
-              <Label>netWorth</Label>
-              <div className="flex flex-row items-center space-x-1">
-                <span>$</span>
-                <Input
-                  value={netWorth}
-                  disabled
-                  className="border-2 border-black w-auto"
-                />
-              </div>
-              <Label>Money Ready For Investment</Label>
-              <div className="flex flex-row items-center space-x-1">
-                <span>$</span>
-                <Input
-                  value={moneyReadyForInvestment}
-                  disabled
-                  className="border-2 border-black w-auto"
-                />
-              </div>
+              <OutputTextBox
+                label="Age"
+                value={age}
+                classNameLabel=""
+                classNameValue=""
+              />
+              <OutputTextBoxDollar
+                label="Net Worth"
+                value={netWorth}
+                classNameLabel=""
+                classNameValue=""
+              />
+              <OutputTextBoxDollar
+                label="Money Ready For Investment"
+                value={moneyReadyForInvestment}
+                classNameLabel=""
+                classNameValue=""
+              />
             </div>
           </AccordionContent>
         </AccordionItem>
