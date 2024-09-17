aceaaf966d26bffb1d9705a2578798492a3a26a4
Peanutt47 <pnchalee@gmail.com>
2024-09-17 12:49:19 +0700

    create OutPutTextBox component

---
+++ components/admin/OutputTextBox.tsx
@@ -0,0 +1,20 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function OutputTextBox({
+    label,
+    value,
+    classNameLabel,
+    classNameValue,
+} : OutputTextBoxProps) {
+  return (
+    <>
+    <Label className={`flex-shrink-0 ${classNameLabel}`}>{label}</Label>
+    <Input
+      value={value}
+      disabled
+      className={`border-2 border-black w-auto ${classNameValue}`}
+    />
+  </>
+  );
+}
