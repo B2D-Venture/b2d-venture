cf27b3ddd5f2c5336faa96344c14c82c081a5805
Peanutt47 <pnchalee@gmail.com>
2024-10-01 13:20:07 +0700

    add text for first index

--- components/main/banner.tsx
+++ components/main/banner.tsx
@@ -10,7 +10,9 @@ import {
 
 import { AspectRatio } from "@/components/ui/aspect-ratio";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+
+import Link from "next/link";
 
 export function BannerCarousel({
   bannerData,
@@ -27,19 +29,40 @@ export function BannerCarousel({
             <CarouselItem key={index}>
               <div className="h-full w-full">
                 {" "}
-                  <div
-                    className="w-full h-[400px] bg-cover bg-center"
-                    style={{
-                      backgroundImage: `url(${banner.backgroundUrl})`,
-                      backgroundSize: "cover",
-                    }}
-                  ></div>
+                <div
+                  className="w-full h-[400px] bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url(${banner.backgroundUrl})`,
+                    backgroundSize: "cover",
+                  }}
+                >
+                  {index === 0 ? (
+                    <div className="pl-8">
+                      <span className="text-on-image-white">
+                        Invest for Future in
+                        <br></br>
+                        Stable Platform and
+                      </span>
+                      <br></br>
+                      <span className="text-on-image-orange">
+                        Make Fast Money
+                      </span>
+                    </div>
+                  ) : null}
+                </div>
               </div>
             </CarouselItem>
           ))}
         </CarouselContent>
         {/* Optionally uncomment these for navigation */}
-        <Button className="absolute bottom-4 left-4 transform translate-y-0 border border-input bg-[#E8A117] shadow-sm hover:bg-accent hover:text-accent-foreground text-black">Register</Button>
+        <Link href="/role-register">
+          <Button
+            size="lg"
+            className="absolute bottom-8 left-8 transform translate-y-0 border border-input bg-[#E8A117] shadow-sm hover:bg-accent hover:text-accent-foreground text-black"
+          >
+            Register
+          </Button>
+        </Link>
         <CarouselPrevious className="left-4 top-1/2 transform -translate-y-1/2" />
         <CarouselNext className="right-4 top-1/2 transform -translate-y-1/2" />
       </Carousel>
