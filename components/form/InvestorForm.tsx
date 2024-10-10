05d625c8a355ac1873663987681139b3ad4cab8c
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-10-10 18:22:35 +0700

    Adjust step display design

--- components/form/InvestorForm.tsx
+++ components/form/InvestorForm.tsx
@@ -15,10 +15,11 @@ export default function InvestorForm() {
             Investor Profile
           </h1>
         </div>
-        <div className="w-full flex justify-center mb-8">
-          <ShowStep step={2} />
-        </div>
-        <div className="flex w-11/12 h-9/10 bg-[#D9D9D9] rounded-[10px] justify-center items-center p-[57px]">
+
+        <div className="bg-[#d9d9d9] rounded-[30px] relative p-10 mt-5 w-full max-w-7xl">
+          <div className="w-full flex justify-center mb-8">
+            <ShowStep step={2} />
+          </div>
           <InvestorRegisterForm />
         </div>
       </div>
