3b7d467b4d5f53391ae3ceb8bbc37ef145f0bebf
Kantaphat PHAPHUI <bosskingblack10@gmail.com>
2024-10-14 18:22:46 +0700

    Display a waiting message for approval in the component

--- app/(root)/investor-profile/page.tsx
+++ app/(root)/investor-profile/page.tsx
@@ -5,7 +5,7 @@ import { getUserByEmail, getInvestorById } from "@/lib/db/index";
 import { getServerSession } from "next-auth";
 import { authConfig } from "@/lib/auth";
 import { redirect } from "next/navigation";
-import { AiOutlineClockCircle } from "react-icons/ai";
+import WaitingShow from "@/components/profile/WaitingShow";
 
 export default async function InvestorProfile() {
   const session = await getServerSession(authConfig);
@@ -29,24 +29,7 @@ export default async function InvestorProfile() {
     <div>
       <div className="flex flex-col items-center min-h-screen relative">
         {/* Overlay for accounts under review */}
-        {investor?.status === false && (
-          <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-70 z-20 p-4">
-            <div className="bg-[#ffffffe6] rounded-lg text-center p-8 shadow-lg border-4 border-[#FABC3F]">
-              {/* Centered Animated Icon */}
-              <div className="flex justify-center mb-4 animate-bounce">
-                <AiOutlineClockCircle className="text-[#FFAD60] text-[80px]" />
-              </div>
-
-              {/* Title */}
-              <h1 className="text-[42px] font-bold text-[#FFAD60] mb-2">Not Approved Yet</h1>
-
-              {/* Optional: Add a subtle fade-in animation */}
-              <p className="text-[28px] text-[#A66E38]">
-                Waiting for admin to approve your information...
-              </p>
-            </div>
-          </div>
-        )}
+        {investor?.status === false && (<WaitingShow />)}
 
         <div className="flex justify-between w-11/12 h-9/10">
           <h1 className="flex flex-col justify-center text-white ml-70 text-[40px] font-bold mt-10">
---
+++ components/profile/WaitingShow.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { AiOutlineClockCircle } from "react-icons/ai";
+
+const WaitingShow = () => {
+    return (
+        <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-70 z-20 p-4">
+            <div className="bg-[#ffffffe6] rounded-lg text-center p-8 shadow-lg border-4 border-[#FABC3F]">
+                {/* Centered Animated Icon */}
+                <div className="flex justify-center mb-4 animate-bounce">
+                    <AiOutlineClockCircle className="text-[#FFAD60] text-[80px]" />
+                </div>
+
+                <h1 className="text-[42px] font-bold text-[#FFAD60] mb-2">Not Approved Yet</h1>
+
+                <p className="text-[28px] text-[#A66E38]">
+                    Waiting for admin to approve your information...
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default WaitingShow
